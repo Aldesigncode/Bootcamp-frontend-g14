fc8264e789d49d1520cf3ab27ef7aa085e4b34e1
unknown <alexandgod@hotmail.com>
2023-07-07 19:49:13 -0500

    Actualizando proyectos

---
+++ Semana 5/día02-APRENDIENDOJS/app.js
@@ -0,0 +1,33 @@
+console.log('Hola chicos soy el javascript')
+console.log('Hola G14')
+console.log(2+2)
+
+
+//Tipos de datos en javascript
+//Number = operadores aritméticos
+console.log(100 + 4);
+consoles.log(100 + 4);
+console.log(100/4)
+//módulo = residuo
+
+console.log(typeof(10%3));
+
+//string = operador unitario
+consoles.log(typeof("Hola, mi nombre es Mariana"));
+
+// typeof()
+
+//Booleans = opeadores comparativos (>, <, =)
+
+// true(verdadero)
+//false(false)
+
+consoles.log(typeof(3 < 5));
+console.log(10<15);
+
+// CONVERSIÓN AUTOMÁTICA = COERCIÓN DE TIPO
+
+console.log('5'-1);
+console.log(typeof('5'+1));
+
+//VARIABLES
